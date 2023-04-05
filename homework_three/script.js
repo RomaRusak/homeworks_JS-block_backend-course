e7f6636d796e25019f0bbbe492eb9d5953f478cf
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2023-04-05 13:35:41 +0300

    tasks 1 - 5 added

--- homework_three/script.js
+++ homework_three/script.js
@@ -1,38 +1,99 @@
+//Задача1
+
+console.log('числа от 1 до 50:')
+for (let i = 1; i <= 50; i++) {
+    console.log(i)
+}
+
+console.log('числа от 35 до 8:')
+for (let i = 35; i >= 8; i--) {
+    console.log(i)
+}
+
+//Задача2
+
+
+const task2 = (i) => {
+    while ( i >= 11) {
+    const div = document.createElement('div')
+    div.innerText = i
+    document.body.append(div)
+    i--
+}
+}
+
+task2 (89)
+
+//Задача 3
+
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+}
+
+console.log(`сумма чисел от 0 до 100 = ${sum}`);
+
+//Задача 4
+
+
+for (let i = 1; i <= 5; i++) {
+  let sum = 0;
+
+  for (let j = 1; j <= i; j++) {
+    sum += j;
+  }
+
+  console.log(`сумма чисел числа ${i} = ${sum} `);
+}
+
+//Задача 5
+
+
+console.log('решение 5 задачи через цикл for');
+for (let i = 8; i <= 56; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+
+console.log('решение 5 задачи через цикл while');
+let i = 8;
+while (i <= 56) {
+  if (i % 2 === 0) console.log(i);
+  i++;
+}
+
 //Задача 6
 
+
 for (let i = 2; i <= 10; i++) {
-    
-    for (let j = 1; j <= 10; j++) {
-
-        console.log(`${i} * ${j} = ${i * j}`)
-        if (j === 10) console.log('...')
-        
-    }
-
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    if (j === 10) console.log('...');
+  }
 }
 
 // Задача 7
 
+
 const task7 = (number) => {
+  if (Number.isNaN(+number)) return 'вы ввели не число';
 
-    if (Number.isNaN(+number)) return 'вы ввели не число'
-    
+  let counter = 0;
 
-    let counter = 0
+  for (; 1; ) {
+    number /= 2;
+    counter++;
+    if (number < 50) break;
+  }
 
-    for (; 1 ; ) {
-        number /= 2
-        counter++
-        if (number < 50) break
-    }
+  return `чисало:${number}, необходимое колличество итераций: ${counter}`;
+};
 
-    return `чисало:${number}, необходимое колличество итераций: ${counter}`
-
-}
-
-console.log(task7(1000))
+console.log(task7(1000));
 
 //Задача 8
+
 
 const task8 = () => {
   let sum = 0;
@@ -59,6 +120,7 @@ const task8 = () => {
 // console.log(task8());
 
 // Задача 9
+
 
 const task9 = (string) => {
   let min = Infinity;
@@ -100,6 +162,7 @@ const task9 = (string) => {
 console.log(task9('asdas4 8 15 16 23 42.5'));
 
 // Задача 10
+
 
 const task10 = (number) => {
   if (!Number.isInteger(number)) return 'вы ввели не число';
